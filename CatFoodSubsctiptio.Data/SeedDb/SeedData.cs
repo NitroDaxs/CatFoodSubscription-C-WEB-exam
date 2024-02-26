ad9f6bca9a4fd2e6f772912fff7b25d0f0a305ea
NitroDaxs <NitroDaxs@students.softuni.bg>
2024-02-27 00:29:27 +0200

    Started to write the seeding of the database

---
+++ CatFoodSubsctiptio.Data/SeedDb/SeedData.cs
@@ -0,0 +1,38 @@
+﻿using CatFoodSubscription.Data.Models;
+
+namespace CatFoodSubscription.Data.SeedDb
+{
+    internal class SeedData
+    {
+        public Category FoodCategory { get; set; }
+        public Category SupplementCategory { get; set; }
+        public Category ToyCategory { get; set; }
+
+
+        public SeedData()
+        {
+
+        }
+
+        private void SeedCategories()
+        {
+            FoodCategory = new Category()
+            {
+                Id = 1,
+                Name = "Food"
+            };
+
+            SupplementCategory = new Category()
+            {
+                Id = 2,
+                Name = "Supplement"
+            };
+
+            ToyCategory = new Category()
+            {
+                Id = 3,
+                Name = "Toy"
+            };
+        }
+    }
+}
