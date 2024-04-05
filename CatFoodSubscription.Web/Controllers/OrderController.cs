46a0cc5031a75d60212672265999429b5b1be3f6
NitroDaxs <NitroDaxs@students.softuni.bg>
2024-04-05 18:58:00 +0300

    Added an Error messege and a logger messege and implemented try catch in the methods

--- CatFoodSubscription.Web/Controllers/OrderController.cs
+++ CatFoodSubscription.Web/Controllers/OrderController.cs
@@ -10,35 +10,57 @@ namespace CatFoodSubscription.Web.Controllers
     {
         private readonly IOrderService orderService;
         private readonly ISubscriptionBoxService subscriptionBoxService;
+        private readonly ILogger<OrderController> logger;
 
-        public OrderController(IOrderService _orderService, ISubscriptionBoxService _subscriptionBoxService)
+        public OrderController(IOrderService _orderService, ISubscriptionBoxService _subscriptionBoxService, ILogger<OrderController> _logger)
         {
             orderService = _orderService;
             subscriptionBoxService = _subscriptionBoxService;
+            logger = _logger;
         }
+
+        /// <summary>
+        /// This method returns the product cart.
+        /// </summary>
         [HttpGet]
         public async Task<IActionResult> Index()
         {
             try
             {
                 var summary = await orderService.GetOrderSummaryAsync(User.GetId());
-
                 return View(summary);
             }
             catch (Exception)
             {
+                TempData["ErrorMessage"] = errorNotification;
+                logger.LogError($"Error has occurred while trying to fetch a order summary {0}", DateTime.Now);
                 return RedirectToAction("Index", "SubscriptionBox");
             }
         }
 
+        /// <summary>
+        /// This method is responsible for the increasing or decreasing product quantity in the cart.
+        /// </summary>
         [HttpPost]
         public async Task<IActionResult> UpdateQuantity(int productId, string action)
         {
-            await orderService.UpdateProductQuantityAsync(productId, action, User.GetId());
+            try
+            {
+                await orderService.UpdateProductQuantityAsync(productId, action, User.GetId());
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                TempData["ErrorMessage"] = errorNotification;
+                logger.LogError($"Error has occurred while trying to fetch a order summary {0}", DateTime.Now);
+                return RedirectToAction("Index");
+            }
 
-            return RedirectToAction("Index");
         }
 
+        /// <summary>
+        /// This method retuns the CheckOut form for the current order.
+        /// </summary>
         [HttpGet]
         public async Task<IActionResult> CheckOut()
         {
@@ -52,6 +74,9 @@ namespace CatFoodSubscription.Web.Controllers
             return View(summary);
         }
 
+        /// <summary>
+        /// This method is responsible for taking the CheckOut information and processing the order.
+        /// </summary>
         [HttpPost]
         public async Task<IActionResult> CheckOut(OrderCheckOutFormViewModel model)
         {
@@ -63,10 +88,23 @@ namespace CatFoodSubscription.Web.Controllers
                 return View("CheckOut", summary);
             }
 
-            await orderService.ProcessOrderAsync(model, userId);
-            return RedirectToAction("Index", "SubscriptionBox");
+            try
+            {
+                await orderService.ProcessOrderAsync(model, userId);
+                return RedirectToAction("Index", "SubscriptionBox");
+            }
+            catch (Exception)
+            {
+                TempData["ErrorMessage"] = errorNotification;
+                logger.LogError($"Error has occurred while trying to fetch a order summary {0}!", DateTime.Now);
+                return RedirectToAction("Index", "Order");
+            }
+
         }
 
+        /// <summary>
+        /// This method is responsible for adding products to the cart.
+        /// </summary>
         [HttpPost]
         public async Task<IActionResult> AddToCart(int productId)
         {
@@ -87,46 +125,96 @@ namespace CatFoodSubscription.Web.Controllers
             catch (Exception)
             {
                 TempData["ErrorMessage"] = errorNotification;
+                logger.LogError($"Error has occurred while trying to add a product to cart {0}!", DateTime.Now);
                 return RedirectToAction("Index", "SubscriptionBox");
             }
         }
 
-        //Action to add a subscription box to the cart
+        /// <summary>
+        /// This method is responsible for adding subscription box to the cart.
+        /// </summary>
         [HttpPost]
         public async Task<IActionResult> AddToCartSubscription(int subscriptionBoxId)
         {
-            var subscriptionBox = await subscriptionBoxService.GetByIdAsync(subscriptionBoxId);
-
-            if (subscriptionBox != null)
+            try
             {
-                await orderService.AddSubscriptionBoxToCartAsync(subscriptionBox, User.GetId());
-            }
+                var subscriptionBox = await subscriptionBoxService.GetByIdAsync(subscriptionBoxId);
 
-            return RedirectToAction("Index", "Order"); // Redirect to the cart page
+                if (subscriptionBox != null)
+                {
+                    await orderService.AddSubscriptionBoxToCartAsync(subscriptionBox, User.GetId());
+                }
+
+                return RedirectToAction("Index", "Order"); // Redirect to the cart page
+            }
+            catch (Exception)
+            {
+                TempData["ErrorMessage"] = errorNotification;
+                logger.LogError($"Error has occurred while trying to add a subscription box to cart! {DateTime.Now}");
+                return RedirectToAction("Index", "SubscriptionBox");
+            }
         }
 
 
+        /// <summary>
+        /// This method is responsible for removing the subscription box from the cart.
+        /// </summary>
         [HttpPost]
         public async Task<IActionResult> RemoveSubscriptionBox(int orderId)
         {
-            await orderService.RemoveSubscriptionBoxAsync(orderId);
+            try
+            {
+                await orderService.RemoveSubscriptionBoxAsync(orderId);
 
-            return RedirectToAction("Index");
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                TempData["ErrorMessage"] = errorNotification;
+                logger.LogError($"Error has occurred while trying to remove a subscription box to cart! {DateTime.Now}");
+                return RedirectToAction("Index", "SubscriptionBox");
+            }
         }
 
+
+        /// <summary>
+        /// This method fetches all of the customer's orders.
+        /// </summary>
         [HttpGet]
         public async Task<IActionResult> Orders()
         {
-            var orders = await orderService.GetAllOrdersByIdAsync(User.GetId());
-            return View(orders);
+            try
+            {
+                var orders = await orderService.GetAllOrdersByIdAsync(User.GetId());
+                return View(orders);
+            }
+            catch (Exception)
+            {
+                TempData["ErrorMessage"] = errorNotification;
+                logger.LogError($"Error has occurred while trying to fetch all of the customer's orders! {DateTime.Now}");
+                return RedirectToAction("Index", "SubscriptionBox");
+            }
+
         }
 
-        [HttpPost]
+        /// <summary>
+        /// This method fetches the wanted order.
+        /// </summary>
+        [HttpGet]
         public async Task<IActionResult> Summary(int id)
         {
-            var order = await orderService.OrderSummaryAsync(id);
+            try
+            {
+                var order = await orderService.OrderSummaryAsync(id);
 
-            return View(order);
+                return View(order);
+            }
+            catch (Exception e)
+            {
+                TempData["ErrorMessage"] = errorNotification;
+                logger.LogError($"Error has occurred while trying to fetch one of the customer's orders! {DateTime.Now}");
+                return RedirectToAction("Orders", "Order");
+            }
         }
     }
 }
